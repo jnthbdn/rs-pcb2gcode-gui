8c6ae3a39a485d582a15aa591701839b4b512fcf
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-08-23 09:59:55 +0200

    Add new objects

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,10 +1,7 @@
-// pub mod custom_object;
-// pub mod window_main;
-// pub mod window_tool_db;
-
 pub mod frame {
     pub mod frame_common;
     pub mod frame_input_output;
+    pub mod frame_mill;
     pub mod frame_tool_settings;
     pub mod frame_tree_tools;
 }
@@ -15,6 +12,7 @@ pub mod object {
     pub mod browse_file_object;
     pub mod db_label_object;
     pub mod entry_object;
+    pub mod select_tool_object;
     pub mod spin_button_object;
     pub mod textview_object;
 }
