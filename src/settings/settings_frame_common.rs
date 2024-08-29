b14349036658b491c002c458bef909c92e1d7919
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-08-29 10:42:32 +0200

    set default value

--- src/settings/settings_frame_common.rs
+++ src/settings/settings_frame_common.rs
@@ -1,7 +1,7 @@
 use getset::{CopyGetters, Setters};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, CopyGetters, Setters, Default, Serialize, Deserialize)]
+#[derive(Debug, CopyGetters, Setters, Serialize, Deserialize)]
 pub struct SettingsFrameCommon {
     #[serde(default = "super::true_default")]
     #[getset(get_copy = "pub", set = "pub")]
@@ -60,50 +60,23 @@ pub struct SettingsFrameCommon {
     pub(super) is_zero_start: bool,
 }
 
-// impl SettingsWindow {
-//     pub(crate) fn from_keyfile(key_file: &gtk::glib::KeyFile) -> Self {
-//         Self {
-//             is_input_metric: key_file
-//                 .boolean("common_frame", "is_input_metric")
-//                 .unwrap_or(true),
-
-//             is_output_metric: key_file
-//                 .boolean("common_frame", "is_output_metric")
-//                 .unwrap_or(true),
-
-//             safe_z: Self::get_float(key_file, 0.0, "safe_z"),
-
-//             tool_change: Self::get_float(key_file, 0.0, "tool_change"),
-
-//             tolerance: Self::get_float(key_file, 0.0, "tolerance"),
-
-//             optimization: Self::get_float(key_file, 0.0, "optimization"),
-
-//             tile_x: key_file.uint64("common_frame", "width").unwrap_or(1),
-
-//             tile_y: key_file.uint64("common_frame", "width").unwrap_or(1),
-
-//             offset_x: Self::get_float(key_file, 0.0, "offset_x"),
-
-//             offset_y: Self::get_float(key_file, 0.0, "offset_y"),
-
-//             mirror_x: Self::get_float(key_file, 0.0, "mirror_x"),
-
-//             is_mirror_y: key_file
-//                 .boolean("common_frame", "is_mirror_y")
-//                 .unwrap_or(true),
-
-//             is_zero_start: key_file
-//                 .boolean("common_frame", "is_zero_start")
-//                 .unwrap_or(true),
-//         }
-//     }
-
-//     fn get_float(key_file: &gtk::glib::KeyFile, default: f64, name: &str) -> f64 {
-//         key_file
-//             .string("common_frame", name)
-//             .unwrap_or(default.to_string().into())
-//             .parse::<f64>()
-//             .unwrap_or(default)
-//     }
-// }
+impl Default for SettingsFrameCommon {
+    fn default() -> Self {
+        Self {
+            is_input_metric: true,
+            is_output_metric: true,
+            safe_z: Default::default(),
+            tool_change: Default::default(),
+            is_tool_change_as_machine_coord: Default::default(),
+            tolerance: Default::default(),
+            optimization: Default::default(),
+            tile_x: 1.0,
+            tile_y: 1.0,
+            offset_x: Default::default(),
+            offset_y: Default::default(),
+            mirror_x: Default::default(),
+            is_mirror_y: Default::default(),
+            is_zero_start: Default::default(),
+        }
+    }
+}
